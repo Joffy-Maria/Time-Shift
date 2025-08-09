82caa4e608da9b9f348da293d52ae7791450e763
Joffy-Maria <joffymariapius06@gmail.com>
2025-08-09 07:26:16 +0530

    new change

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["time-shift.onrender.com"], // 👈 Add this line
   },
   plugins: [
     react(),
